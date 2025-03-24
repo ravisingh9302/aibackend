b13612c1db07fca8b27cb3ea660102b62a7d54e8
Jatin Sahu <114304101+jatinsahu0410@users.noreply.github.com>
2025-03-25 01:55:08 +0530

    no change

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,12 +1,11 @@
 import express from 'express';
+import { InterviewRouter } from '../app/interview/interview.routes';
 // import { AuthRouter } from '../app/auth/auth.routes';
 // import { AdminRouter } from '../app/admin/admin.routes';
 // import { TeacherRouter } from '../app/teacher/teacher.routes';
 // import { UserRouter } from '../app/student/student.routes';
 // import { PublicRouter } from '../app/public/public.routes';
 const router = express.Router();
-
-router.use('/interview', InterviewRouter);
 
 router.use('/interview', InterviewRouter);
 
